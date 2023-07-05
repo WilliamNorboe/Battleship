bd3138d007592dc25c6d878151781b3f21ba1867
William Norboe <william.norboe@gmail.com>
2023-07-05 08:07:43 -0700

    split index.js into separate files

---
+++ src/battleship.js
@@ -0,0 +1,71 @@
+// let playerboard = document.querySelector("#playerboard");
+// let aiboard = document.querySelector("#aiboard");
+let boards = document.querySelector(".boards");
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function renderBoards(human, ai){
+    let playerboard = document.createElement("div");
+    playerboard.id = "playerboard";
+    playerboard.textContent = "Your Boats";
+    let aiboard = document.createElement("div");
+    aiboard.id = "aiboard";
+    aiboard.textContent = "Opponents Boats";
+    let opponentDiv = document.createElement("div");
+
+    if(ai.gameboard.allSunk()){
+        document.querySelector("#result").textContent = "Player 1 Win";
+        ai.turn = false;
+        human.turn = false;
+    }
+    else if(human.gameboard.allSunk()){
+        document.querySelector("#result").textContent = "Player 2 Win";
+        human.turn = false;
+        ai.turn = false;
+    }
+    ai.attack(human);
+    for(let i = 0; i < human.gameboard.board.length; ++i){
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for(let j = 0; j < human.gameboard.board[i].length; ++j){
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.id = "hsquare" + String(i) + String(j);
+            square.textContent = human.gameboard.board[i][j];
+            row.appendChild(square);
+        }
+        playerboard.appendChild(row);
+    }
+    
+    ai.attack(human);
+    for(let i = 0; i < ai.gameboard.board.length; ++i){
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for(let j = 0; j < ai.gameboard.board[i].length; ++j){
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.id = "asquare" + String(i) + String(j);
+            if(ai.gameboard.board[i][j] != "*" && ai.gameboard.board[i][j] != "H" && ai.gameboard.board[i][j] != "M"){
+                square.textContent = "*";
+            }
+            else{square.textContent = ai.gameboard.board[i][j];}
+            square.addEventListener("click", ()=>{
+                human.attack(ai, square.id);
+                removeAllChildNodes(boards);
+                renderBoards(human, ai);
+            });
+            row.appendChild(square);
+        }
+        aiboard.appendChild(row);
+    }
+    boards.appendChild(playerboard);
+    boards.appendChild(aiboard);
+}
+
+export{
+    renderBoards
+}
---
+++ src/placeboats.js
@@ -0,0 +1,116 @@
+import { Player } from './player';
+import { GameBoard } from './gameboard';
+import { renderBoards } from './battleship';
+
+let boards = document.querySelector(".boards");
+let orientationButton = document.querySelector("#orientation");
+
+
+let boats = [5, 4, 3, 3, 2];
+
+// ai.gameboard.placeShip(4,5,5,"h");
+// ai.gameboard.placeShip(1,5,5,"h");
+// ai.gameboard.placeShip(3,2,7,"h");
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+
+function placeBoats(human, ai){
+
+    let ori = "v";
+    orientationButton.addEventListener("click", ()=>{
+        if(ori == "v"){
+            ori = "h";
+        }
+        else{
+            ori = "v";
+        }
+    });
+    let playerboard = document.createElement("div");
+    playerboard.textContent = "Place Your boats";
+    playerboard.id = "playerboard";
+    for(let i = 0; i < human.gameboard.board.length; ++i){
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for(let j = 0; j < human.gameboard.board[i].length; ++j){
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.id = "hsquare" + String(i) + String(j);
+            square.addEventListener("mouseover", ()=>(hoverBoat(square.id,ori,"green")));
+            square.addEventListener("mouseout", ()=>(hoverBoat(square.id,ori,"white")));
+            square.addEventListener("click", ()=>(placeBoat(square.id,ori,human,ai)));
+            square.textContent = human.gameboard.board[i][j];
+            row.appendChild(square);
+        }
+        playerboard.appendChild(row);
+    }
+    boards.appendChild(playerboard);
+}
+
+function placeBoat(id, orientation, human,ai){
+    // console.log(id);
+    let row = Number(id[7]);
+    let col = Number(id[8]);
+    console.log(human.gameboard);
+    if(human.gameboard.placeShip(row,col,boats[0],orientation)){
+        aiPlaceBoat(ai);
+        removeAllChildNodes(boards);
+        placeBoats(human, ai);
+        boats.shift();
+        if(boats.length == 0){
+            document.getElementById("orientation").remove();
+            removeAllChildNodes(boards);
+            renderBoards(human, ai);
+        }
+    }
+}
+
+function aiPlaceBoat(ai){
+    let row;
+    let col;
+    let orientation;
+    let valid = false;
+    while(!valid){
+        row = Math.floor(Math.random() * 10);
+        col = Math.floor(Math.random() * 10);
+        orientation = Math.floor(Math.random() * 2);
+        if(orientation == 0){
+            orientation = 'v';
+        }
+        else{
+            orientation = 'h';
+        }
+        valid = ai.gameboard.placeShip(row,col,boats[0],orientation);
+    }
+}
+
+function hoverBoat(id, orientation, color){
+    // console.log(id);
+    let row = Number(id[7]);
+    let col = Number(id[8]);
+    if(orientation == "h"){
+        row = Number(id[8]);
+        col = Number(id[7]);
+    }
+    if(row + boats[0] > 10){
+        return;
+    }
+    // document.getElementById(id).style.backgroundColor = "green";
+    for(let i = row; i < row + boats[0]; ++i){
+        let nid = "hsquare" + String(i) + col;
+        if(orientation == "h"){
+            nid = "hsquare" +  col + String(i);
+        }
+        console.log(nid);
+        document.getElementById(nid).style.backgroundColor = color;
+    }
+}
+
+export{
+    placeBoats
+}
